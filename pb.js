f2a32798dd3ebf2fbb25fe36f516376019f936f7
razaizim1 <zim.sarkar5956@gmail.com>
2022-01-18 13:26:24 +0600

    string problem

---
+++ pb.js
@@ -0,0 +1,10 @@
+// find the populated word is present or not!!
+
+var bangladesh = "Bangladesh is highly populated country";
+var lowercase = bangladesh.toLowerCase();
+
+if (lowercase.indexOf("bangladesh") !== -1) {
+    console.log("The word is present");
+} else {
+    console.log("The word is not present")
+}
